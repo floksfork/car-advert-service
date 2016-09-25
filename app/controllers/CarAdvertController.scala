306aab3b7f4c45acfc34fe4c7dcf739bbaf59813
Oleksandr Siyanko <oleksandr.siyanko@gmail.com>
2016-09-25 12:34:18 +0300

    add routs for the web service.

---
+++ app/controllers/CarAdvertController.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import play.api.mvc.Controller
+
+class CarAdvertController extends Controller{
+
+  def index = TODO
+  def read (id: Int) = TODO
+  def create = TODO
+  def update(id: Int) = TODO
+  def delete(id: Int) = TODO
+  def describe = TODO
+}
